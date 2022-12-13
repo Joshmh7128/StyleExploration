0f2d742dee895b8c32e6352f98902d864fe47bf4
Lorg <hikariame100@gmail.com>
2022-12-13 12:49:30 -0500

    rotation

---
+++ Assets/Lorg/Scripts/AssetRotator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AssetRotator : MonoBehaviour
+{
+    [SerializeField] float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.DOLocalRotate(new Vector3(0, 360, 0), time, RotateMode.FastBeyond360).SetRelative(true).SetEase(Ease.Linear);
+    }
+}
